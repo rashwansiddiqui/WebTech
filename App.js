d33f1a782151617be2698bda0f3004c65b25362d
rashwansiddiqui <73246167+rashwansiddiqui@users.noreply.github.com>
2021-03-26 22:34:34 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-const http=require('http')
+const express=require('express')
 
-var dt = require('./myfirstmodule')
-
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-type':'text/plain'})
-    res.write("Name: "+dt.PrintMyName())
-
-    res.end(" \n Killing response object here")
-}
-
-).listen(8080, ()=> {
-    console.log("The server is running on port 8080")
+var app=express()
+//http://localhost:3000/
+app.get('/',(req, res)=>{
+    res.send("Good morning class")
+})
+//http://localhost:3000/Books
+app.get('/Books',(req, res)=>{
+    res.send("We welcome you to our books section")
 
 })
+//http://localhost:3000/Students
+app.get('/Students',(req,res)=>{
+    res.send("<h1>We welcome you to students section</h1>")
 
-
+})
+app.post('/',(req, res)=>{
+    res.send("You just sent a message to the server")
+})
+//http://localhost:3000/Students/4/books/70
+app.get('Students/:studentid/books/:bookid',(req,res)=>{
+    res.send(req.params)
+})
+var server=app.listen(3000, ()=> {
+    console.log("Server is running on port 3000")
+})
